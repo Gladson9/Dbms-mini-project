af7f9990277359bcd41a390f458c1cc1c94ee358
Gladson <gladsondsouza77@gmail.com>
2019-11-18 20:01:00 +0530

    Update connect.php

--- DB/connect.php
+++ DB/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","login_test") or die("Unable to connect");
+$con = mysqli_connect("localhost","root","","event-management") or die("Unable to connect");
 // mysqli_select_db("login_test",$con);
 
 ?>
