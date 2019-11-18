1e5228f5b7562d2c9a47abbe7984adbf40db59e3
Gladson <gladsondsouza77@gmail.com>
2019-11-18 15:21:04 +0530

    added files and updated

---
+++ DB/connect.php
@@ -0,0 +1,6 @@
+<?php
+
+$con = mysqli_connect("localhost","root","","login_test") or die("Unable to connect");
+// mysqli_select_db("login_test",$con);
+
+?>
---
+++ login.php
@@ -0,0 +1,144 @@
+<?php
+    require 'DB/connect.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./CSS/login.css">
+    <title>Login/Signup</title>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="box" id="login">
+            <div class="login-form">
+                <h1>Login</h1>
+                <br>
+                <br>
+                <form action="login.php" method="post">
+                    <input type="text" name="usernm" class="inp" placeholder="Username" required>
+                    <br>
+                    <input type="password" name="pass" class="inp" placeholder="Password" required>
+                    <br><br>
+
+
+                    <input type="radio" name="user-type" value="admin" class="radio-btn" checked style=" margin: 0;height: 20px;width: 20px;">&nbsp; <span style="color:#86c323">Admin</span> &nbsp;&nbsp;
+
+                    <input type="radio" name="user-type" value="organizer" class="radio" style=" margin: 0;height: 20px;width: 20px;"> &nbsp; <span style="color:#86c323">Orgainzer</span>&nbsp;&nbsp;
+
+                    <input type="radio" name="user-type" value="student" class="radio" style=" margin: 0;height: 20px;width: 20px;">&nbsp; <span  style="color:#86c323">Student</span>
+                    <br>
+                    <br>
+                    <button class="btn" name="login-btn">Login</button>
+                </form>
+
+                <?php
+                    if(isset($_POST['login-btn'])){
+                        $usernm = $_POST['usernm'];
+                        $pass = $_POST['pass'];
+                        $user_type = $_POST['user-type'];
+                        $quer = "SELECT * from register WHERE username='$usernm' AND password='$pass' AND user_type='$user_type'";
+                        $query_ru = mysqli_query($con,$quer);
+                        if(mysqli_num_rows($query_ru)>0){
+                            $_SESSION['usernm']=$usernm;
+                            if($user_type == 'admin'){
+
+                                header('location:admin.php');
+                            }
+                            else if($user_type == 'organizer'){
+                                header('location:organizer.php');
+                            }
+                            else if($user_type == 'student'){
+                                header('location:student.php');
+                            }
+                        }
+                        else
+                        {
+                            echo'<script type="text/javascript"> alert("invalid")</script>';
+                        }
+                    }
+                ?>
+
+            </div>
+        </div>
+        <div class="box" id="signup">
+            <div class="signup-form">
+                <h1>Signup</h1>
+                <br>
+                <br>
+                <form action="login.php" method="post">
+                    <input type="text" name="name" class="input" placeholder="Name" required>
+                    <br>
+                    <input type="text" name="username" class="input" placeholder="Username" required>
+                    <br>
+                    <input type="text" name="email" class="input" placeholder="Email" required>
+                    <br>
+                    <input type="password" name="password" class="input" placeholder="Password" required>
+                    <br>
+                    <input type="password" name="cpassword" class="input" placeholder="Confirm Password" required>
+                    <br>
+                    <input type="number" name="contact" class="input contact" placeholder="Contact Number" required>
+                    <br>
+                    <input type="radio" name="gender" value="Male" checked>Male &nbsp &nbsp
+                    <input type="radio" name="gender" value="Female">Female <br>
+                    <input type="text" name="address" class="input" placeholder="Address" required>
+                    <br>
+                    <input type="radio" name="user-type-r" value="student" checked>Student &nbsp
+                    <input type="radio" name="user-type-r" value="organizer">Orgainzer 
+                    <br>
+                    <br>
+                    <!-- <input type="submit" name="signup-btn" value="SignUp"> -->
+                    <button class="btn" id="ba" name="signup-btn">Signup</button>
+                </form>
+                <?php
+                    if(isset($_POST['signup-btn'])){
+                        // echo '<script type="text/javascript"> alert("button working")</script>';
+                        $name = $_POST['name'];
+                        $username = $_POST['username'];
+                        $email = $_POST['email'];
+                        $password = $_POST['password'];
+                        $cpassword = $_POST['cpassword'];
+                        $contact = $_POST['contact'];
+                        $gender = $_POST['gender'];
+                        $address = $_POST['address'];
+                        $user_type_r = $_POST['user-type-r'];
+
+                        if($password == $cpassword){
+                            
+                            $query = "select * from register WHERE username='$username'";
+                            $query_run = mysqli_query($con,$query);
+
+                            if(mysqli_num_rows($query_run)>0){
+                                echo '<script type="text/javascript"> alert("User already exists..")</script>';
+                            }
+                            {
+                                $query = "insert into register values('$name','$email','$password','$contact','$gender','$address','$username','$user_type_r')";
+                                $query_run = mysqli_query($con,$query);
+                            }
+                            if($query_run){
+                                echo '<script type="text/javascript"> alert("User registered..")</script>';
+
+                            }
+                            else
+                            {
+                                echo ' <script type="text/javascript"> alert("User already exists.. Please Login")</script>';
+                            }
+                        
+                        }
+                        else{
+                            echo ' <script type="text/javascript"> alert("Password does not match")</script>';
+                        }
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
